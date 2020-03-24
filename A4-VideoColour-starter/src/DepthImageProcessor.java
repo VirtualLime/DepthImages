c5b96bf4590dfc797bad55e643e87e0fced17e75
andrewgodbout <agodbout@upei.ca>
2020-03-24 05:33:26 -0300

    Initial commit

---
+++ A4-VideoColour-starter/src/DepthImageProcessor.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+/**
+ * Complete this class as part of the assignment
+ */
+public class DepthImageProcessor implements ImageProcessor{
+
+    @Override
+    public void processFile(String fileString) { //do not throw anything here{
+    }
+
+    @Override
+    public double[][] getRawImg() {
+        return null;
+    }
+
+    @Override
+    public int[][] getColorImg() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Blob> getBlobs() { return null; }
+
+    @Override
+    public void setThreshold(double thres) {
+
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void setMinDist(double minDist) {
+    }
+
+    @Override
+    public void setMaxDist(double maxDist) {
+    }
+}
